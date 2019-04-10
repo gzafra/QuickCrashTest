df43d7825a17514c36fa26c7cb2585637112efe1
Guillermo Zafra <me@gzafra.com>
2019-04-10 10:44:01 +0200

    Initial Commit

---
+++ TestingQuick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingQuick
+//
+//  Created by Guillermo Zafra on 10/04/2019.
+//  Copyright © 2019 Guillermo Zafra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
